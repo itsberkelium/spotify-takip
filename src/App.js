57af74ca98e423850797ea34f03e5dda3087637e
Berke <berke@bucketlist.com.tr>
2020-08-13 19:25:41 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import moment from "moment";
+import "moment/locale/tr";
+import payees from "./payees";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.scss";
+
+export default class App extends Component {
+  month = moment().month();
+  monthName = moment().format("MMMM");
+  nextMonthName = moment().add(1, "M").format("MMMM");
+
+  state = {
+    paysThisMonth: {},
+    paysNextMonth: {},
+  };
+
+  componentDidMount = () => {
+    moment.locale("tr");
+
+    payees.some((data, index) => {
+      if (data.payMonths.includes(this.month)) {
+        this.setState({ paysThisMonth: data }, () => {
+          this.setState({ paysNextMonth: payees[index + 1] });
+        });
+        return true;
+      }
+
+      return false;
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          {this.monthName} ayı Spotify ödeme sırası:{" "}
+          <span>{this.state.paysThisMonth.name}</span>
+        </h1>
+        <h2>
+          Gelecek ay ({this.nextMonthName}):{" "}
+          <span>{this.state.paysNextMonth.name}</span>
+        </h2>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/payees.js
@@ -0,0 +1,10 @@
+const payees = [
+  { id: 0, name: "Berke", payMonths: [11, 5] },
+  { id: 1, name: "Burak", payMonths: [0, 6] },
+  { id: 2, name: "Mert", payMonths: [1, 7] },
+  { id: 3, name: "Buse", payMonths: [2, 8] },
+  { id: 4, name: "Alara", payMonths: [3, 9] },
+  { id: 5, name: "Murat", payMonths: [4, 10] },
+];
+
+export default payees;
